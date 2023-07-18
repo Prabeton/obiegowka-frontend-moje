3ad6c0c39e344d0a30719ee6002fae65bd3822c1
Prabeton <3846205@gmail.com>
2023-07-18 04:16:41 +0200

    refresz3 mozemy zaczynac

---
+++ odzyskiwanieHasla.js
@@ -0,0 +1,1 @@
+// odzyskiwanie hasła.js
---
+++ profile.js
@@ -0,0 +1,3 @@
+// profile.js 
+// będą służyć do obsługi użytkownika niezalogowanego i powiedzenia, że nie ma wstępu
+// jeśli logowanie będzie udane tutaj mamy zobaczyć listę zalogowanych 
